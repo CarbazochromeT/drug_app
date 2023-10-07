1c7dab91c487e352bae81942283e18f2c6546c76
asano_haruka_39 <flare.marionette@gmail.com>
2023-10-07 12:10:07 +0900

    追従ボタン

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -23,5 +23,22 @@ $(document).on('turbo:load', function(){
   $('.like_star').trigger('click');
 });
 
+$(document).on('turbo:load', function() {
+  var offset = 100;
+  var duration = 500;
+  $(window).scroll(function() {
+  if ($(this).scrollTop() > offset) {
+  $('#pagetop').fadeIn(duration);
+  } else {
+  $('#pagetop').fadeOut(duration);
+  }
+  });
+  
+  $('#pagetop').click(function(event) {
+  event.preventDefault();
+  $('html, body').animate({scrollTop: 0}, duration);
+  return false;
+  })
+});
 
 
