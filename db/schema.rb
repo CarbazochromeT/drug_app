7a248be2e0923723f9f159b685716d7d52f16fb5
asano_haruka_39 <flare.marionette@gmail.com>
2023-10-09 17:19:38 +0900

    Gemfile.lockバージョン変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_09_032248) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_09_062338) do
   create_table "drug_ingredients", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "drug_id"
     t.bigint "ingredient_id"
@@ -83,10 +83,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_032248) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "drug_ingredients", "drugs"
-  add_foreign_key "drug_ingredients", "ingredients"
-  add_foreign_key "drug_symptoms", "drugs"
-  add_foreign_key "drug_symptoms", "symptoms"
   add_foreign_key "drugs", "maker_names"
   add_foreign_key "likes", "drugs"
   add_foreign_key "likes", "users"
