84ad6965d15b3d78a31b946605ad55b1469ab999
asano_haruka_39 <flare.marionette@gmail.com>
2023-10-10 17:17:15 +0900

    Zeitwerkをclassicへ

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 module DrugApp5
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.autoloader = :classic 
     config.encoding = "utf-8"
 
     # Configuration for the application, engines, and railties goes here.
