1a7f8f586be816a5314e5d11fa3a7616527e5b88
asano_haruka_39 <flare.marionette@gmail.com>
2023-10-17 00:34:55 +0900

    Userデータ消去

--- db/seeds.rb
+++ db/seeds.rb
@@ -152,9 +152,6 @@ MakerName.create!([
   {name: "ロートニッテン"},
   {name: "昭和製薬・大"}
 ])
-User.create!([
-  {email: "admin@admin.com", crypted_password: "$2a$10$F3NGuIMFzkOJtNC3px5KqODE7DibeQibLSFyBqV1KolOmtWqI2i9i", salt: "rXTVvrLTKnP-i7ZP5o1z", username: "アルセウス", admin: true}
-])
 Symptom.create!([
   {name: "頭痛"},
   {name: "熱"},
