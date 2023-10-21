51b063542daecc99b25a4f987f22664cff3a898e
asano_haruka_39 <flare.marionette@gmail.com>
2023-10-21 10:08:35 +0900

    カラムをstringからintegerに戻す

--- db/migrate/20231021005006_changeformulation.rb
+++ db/migrate/20231021005006_changeformulation.rb
@@ -1,5 +1,5 @@
 class Changeformulation < ActiveRecord::Migration[7.0]
-  def change
+  def down
     change_column :drugs, :formulation, :string
     #Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
