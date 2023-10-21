b8ff8871ad3634227765acda838fb43fa10ad511
asano_haruka_39 <flare.marionette@gmail.com>
2023-10-21 10:53:44 +0900

    integerに変更完了

--- db/migrate/20231021011205_changeformulation3.rb
+++ db/migrate/20231021011205_changeformulation3.rb
@@ -1,5 +1,5 @@
 class Changeformulation3 < ActiveRecord::Migration[7.0]
   def change
-    change_column :drugs, :formulation, :integer
+    change_column :drugs, :formulation,'integer USING CAST(drugs AS integer)'
   end
 end
